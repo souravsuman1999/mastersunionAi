b578814b64d15167ccc4b2ce454c3b7806bef17c
sourav suman <sourav.dcm2000@gmail.com>
2025-12-08 11:35:07 +0530

    etesting

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
   //     {
   //       url: "/icon-light-32x32.png",
   //       media: "(prefers-color-scheme: light)",
-  //     },
+  //     }, 
   //     {
   //       url: "/icon-dark-32x32.png",
   //       media: "(prefers-color-scheme: dark)",
